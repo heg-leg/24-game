72a73e965a9c9f1177b3b841e1916d7cbba9a25a
Layla Gonzalez <laylaglez1301@gmail.com>
2025-04-29 22:06:25 -0400

    Fix flash app definition placement

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 import os
+import random
+
+app = Flask(__name__)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
-app = Flask(__name__)
 
 @app.route("/")
 def index():
